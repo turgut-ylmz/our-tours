b04da0201471434090488bc200e7d93750756c27
Turgut Yılmaz <songulturgut0641@gmail.com>
2022-07-12 01:23:11 +0300

    our-tours

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# our-tours
+## React Projects Starter APP
---
+++ src/Tour.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Tour = ({id,image,info,name,price, removeTour}) => {
+  const [readmore, setReadmore] = useState(false);
+
+  return (
+    <article className='single-tour'>
+      <img src={image} alt="asdfd" />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className='tour-price'>{price}</h4>
+        </div>
+          {/* <p>{readmore ? info : `${info.substring(0,200)}...`} */}
+          <p>{readmore ? info : info.slice(0,200) + "..."}
+          <button onClick={()=> setReadmore(!readmore)}>{readmore ? "show less" : "read more"}</button></p>
+          <button className='delete-btn' onClick={()=> removeTour(id)}>Not Interested</button>
+      </footer>
+    </article>
+  )
+};
+
+export default Tour;
